b397760d334eea997e7f7c376dc08972ac81d743
Nauman Zubair <nauman.zubair@gmail.com>
2021-12-24 13:33:28 +0400

    Update ReverseLinkedList.java

--- leetcode/linkedlist/ReverseLinkedList.java
+++ leetcode/linkedlist/ReverseLinkedList.java
@@ -1,15 +1,16 @@
 
- //Definition for singly-linked list.
- 
-  public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+ //Definition for singly-linked list
  
 class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     public ListNode reverseList(ListNode head) {
         return reverseIterative(head);
     }
