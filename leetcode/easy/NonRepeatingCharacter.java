d7e3e776de5c8ca28f1bd8d7a8aee508b9a029b6
Nauman Zubair <nauman.zubair@gmail.com>
2022-01-19 00:53:33 +0400

    Non Repeating function

---
+++ leetcode/easy/NonRepeatingCharacter.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class NonRepeatingCharacter {
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("aab"));
+    }
+
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            
+            if(!map.containsKey(current)) {
+               map.put(current, i);
+            } else {
+                map.put(current, -1);
+            }                
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(char c : map.keySet()) {
+            int index = map.get(c);
+            if(index != -1 && index < min) {
+                min = index;
+            }
+        }
+        
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
