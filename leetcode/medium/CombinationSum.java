a975b2b6333940b5ee2503ef30d52682c5df8baf
Nauman Zubair <nauman.zubair@gmail.com>
2022-01-24 21:17:10 +0400

    LC # 39 Combination Sum

---
+++ leetcode/medium/CombinationSum.java
@@ -0,0 +1,34 @@
+/*
+    //Combination Sum
+
+    39 : https://leetcode.com/problems/combination-sum/
+*/
+
+
+import java.util.*;
+public class CombinationSum {
+    
+    public static void main(String[] args) {
+        System.out.print(combinationSum(new int[] {2, 3, 6, 7}, 7).toString());
+    }
+    
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList();
+        
+        if(candidates.length == 0) return result;
+        backtrack(candidates, target, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    public static void backtrack(int[] cand, int target, int start, List<Integer> curr, List<List<Integer>> result) { 
+        if(target < 0) return;
+
+        if(target == 0) result.add(new ArrayList(curr));
+
+        for (int i = start; i < cand.length; i++) {
+            curr.add(cand[i]);
+            backtrack(cand, target - cand[i], start, curr, result);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
