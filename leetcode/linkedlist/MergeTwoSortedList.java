fb61141d4de409a7d51288ce006967d2c75ac11b
Nauman Zubair <nauman.zubair@gmail.com>
2022-01-27 23:52:39 +0400

    LC # 21 Merge Sorted Array

--- leetcode/linkedlist/MergeTwoSortedList.java
+++ leetcode/linkedlist/MergeTwoSortedList.java
@@ -1,18 +1,11 @@
 
-
+/*
+    21. Merge Two Sorted Lists    
+    https://leetcode.com/problems/merge-two-sorted-lists/
+*/
 public class MergeTwoSortedList {
     
-    class ListNode  {
-        int val;
-        ListNode next;
-
-        ListNode(int val) { this.val = val;}
-
-        ListNode(int val, ListNode next) { this.val = val; this.next = next;}
-        
-    }
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode mergedList = new ListNode(0);
         ListNode head = mergedList;
         
@@ -37,8 +30,36 @@ public class MergeTwoSortedList {
     }
     
     public static void main(String[] args) {
-        
+        // 10 - 20 - 30
+        // 1 - 2 - 3
+
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+
+        ListNode l2 = new ListNode(10, new ListNode(20, new ListNode(30)));
+
+        ListNode l3 = mergeTwoLists(l1, l2);
+
+        printList(l3);
+
     }
 
+    private static void printList(ListNode output) {
+        if(output == null) return;
+        System.out.print(output.val);
+        if(output.next != null)
+            System.out.print("=>");
 
+        printList(output.next);
+    }
+
+}
+
+class ListNode  {
+    int val;
+    ListNode next;
+
+    ListNode(int val) { this.val = val;}
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+    
 }
