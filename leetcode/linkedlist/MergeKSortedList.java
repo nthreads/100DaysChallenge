ee7f4e1c03df9db416414c82c59fcd063aa25020
Nauman Zubair <nauman.zubair@gmail.com>
2022-01-28 00:02:49 +0400

    LC #23 Merge K Sorted Lists

---
+++ leetcode/linkedlist/MergeKSortedList.java
@@ -0,0 +1,23 @@
+/*
+    23. Merge k Sorted Lists
+    https://leetcode.com/problems/merge-k-sorted-lists/
+*/
+
+public class MergeKSortedList {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode kMergedList = new ListNode(0);
+
+        
+        return kMergedList.next;
+
+    }
+}
+
+class ListNode  {
+    int val;
+    ListNode next;
+
+    ListNode(int val) { this.val = val;}
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}    
+}
